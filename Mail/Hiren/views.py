9e3e27af3655ade49c05415dd2ddb8eae750469a
pyprism <git.pyprism@gmail.com>
2014-11-09 21:42:22 +0600

    simple test

--- Mail/Hiren/views.py
+++ Mail/Hiren/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib import messages
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 # Create your views here.
+
+
+def test(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('/add')
+        else:
+            messages.error(request, 'Username/Password is not valid!')
+            return redirect(request.path)
+    else:
+        return redirect('/add')
