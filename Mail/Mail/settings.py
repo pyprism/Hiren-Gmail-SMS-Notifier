c742f14cd5c5d54c64e2ba730f48fc810fa509e4
pyprism <git.pyprism@gmail.com>
2014-11-09 09:15:38 +0600

    initial settings

--- Mail/Mail/settings.py
+++ Mail/Mail/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'Mail'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -60,8 +62,11 @@ WSGI_APPLICATION = 'Mail.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Gmail',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
